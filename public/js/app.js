97fa54f8575ddaa65fe29f5184dc49770d590ac2
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-20 13:53:26 -0300

    Complete the weather API browser connection

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,20 @@
 console.log('js file loaded')
 
-const url = '/weather?address=natal'
-fetch(url).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
+const weatherForm = document.querySelector('.weather-form')
+const addressInput = document.querySelector('#addressInput')
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const address = addressInput.value
+  const url = '/weather?address=' + address
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.location)
+        console.log(data.forecast)
+      }
+    })
   })
 })
