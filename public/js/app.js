aec62609cf8ba1c5af07afc9b6d4f16b8a535095
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-20 14:06:54 -0300

    Connect UI forms to API

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,27 @@
 console.log('js file loaded')
 
+const firstParagraph = document.querySelector('#first-paragraph')
+const secondParagraph = document.querySelector('#second-paragraph')
+
+firstParagraph.textContent = ''
+secondParagraph.textContent = ''
+
 const weatherForm = document.querySelector('.weather-form')
 const addressInput = document.querySelector('#addressInput')
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
+  firstParagraph.textContent = 'Loading...'
+  secondParagraph.textContent = ''
 
   const address = addressInput.value
   const url = '/weather?address=' + address
   fetch(url).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error)
+        firstParagraph.textContent = data.error
       } else {
-        console.log(data.location)
-        console.log(data.forecast)
+        firstParagraph.textContent = data.location
+        secondParagraph.textContent = data.forecast
       }
     })
   })
