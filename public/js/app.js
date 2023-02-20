79df9d1a8d486db469d95ae78842855c02ccfcbb
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-20 13:34:45 -0300

    Access data in browser js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('js file loaded')
+
+const url = '/weather?address=natal'
+fetch(url).then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data.location)
+      console.log(data.forecast)
+    }
+  })
+})
